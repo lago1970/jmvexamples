49a01f85ffc2f7e512fbbbc52ed34bfc6e8e016f
Jonathon Love <jon@thon.cc>
2016-11-17 18:31:02 -0500

    Initial work

---
+++ R/example01.b.R
@@ -0,0 +1,14 @@
+
+Example01Class <- R6::R6Class(
+    "Example01Class",
+    inherit = Example01Base,
+    private = list(
+        .init = function() {
+            preformatted <- jmvcore::Preformatted$new(self$options, 'results')
+            self$results$add(preformatted)
+        },
+        .run = function() {
+            results <- self$results$get('results')
+            results$content <- 'The fish was delish'
+        })
+)
